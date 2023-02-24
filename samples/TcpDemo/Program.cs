2dad9e5e9123ad4ec43cfa2a6ef199e1c99d10e9
Victor.X.Qu <fs7744@hotmail.com>
2023-02-24 22:33:32 +0800

    init

---
+++ samples/TcpDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Zzz.Host;
+
+namespace TcpDemo
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<HostedService>();
+                })
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
